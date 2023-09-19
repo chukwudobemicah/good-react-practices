bcbf1ef80b866111ed264cf8fc1a819400c023af
Chukwudobe Micah <dobemike101@gmail.com>
2023-09-19 21:15:22 +0100

    good react practices

---
+++ README.md
@@ -0,0 +1,5 @@
+## REACT GOOD PRACTICES.
+
+## LESSON 1
+
+<p>Imports should be absolute (@/) and not relative(../)</p>
---
+++ pages/badPractice.js
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar"; // wrong way
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
