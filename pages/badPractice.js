e0d952c6d2b74469b22f08612faa51671336a611
Chukwudobe Micah <dobemike101@gmail.com>
2023-09-21 19:40:22 +0100

    useHover hook

--- pages/badPractice.js
+++ pages/badPractice.js
@@ -1,10 +1,1 @@
-import { useEffect, useState } from "react";
-
-export default function App({ dashboardId }) {
-  const [note, setNote] = useState("");
-  useEffect(() => {
-    setNote("");
-  }, []);
-
-  return <div>{note}</div>;
-}
+// check good practice
